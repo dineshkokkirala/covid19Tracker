efc9f2fa099137fc77090249b333ba92c7ed4693
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2021-04-26 19:41:45 +0530

    Half Done

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="text-center p-4 bg-blue-800 text-white mb-10">
+    <div class="text-3xl md:text-5xl mb-3 font-bold"> <i class="fa fa-viruses"></i> Covid-19 Tracker</div>
+    <p>API by <a href="https://covid19api.com" class="text-blue-300" target="_blank">covid19api</a> </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
